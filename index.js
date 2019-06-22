5afc45f641d4f76cdc5fed825747bfb60c47f712
newtypeV2 <gianlibunao@gmail.com>
2019-06-22 18:30:36 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+// const divide = ()=> 2000 / 100
+
+function divide(){
+    return 2000 / 100;
+}
+
+
+const square = number => number**2;
+
+const add = (parameter1,parameter2) => parameter1+parameter2;
